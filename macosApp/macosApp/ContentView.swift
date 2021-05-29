6cca04f432c538e35c36043e9121385703290eda
pagr0m <pagrom27@gmail.com>
2021-05-30 01:54:47 +0200

    Update example

--- macosApp/macosApp/ContentView.swift
+++ macosApp/macosApp/ContentView.swift
@@ -7,11 +7,9 @@ struct ContentView: View {
         
         // Kotlin code call examples
         let hello = network.hello()
-        let manager = network.manager()
-        let status = String(network.status())
-        let requestValue = network.restGet()
+        let response = network.restGet()
 
-        Text(requestValue).padding()
+        Text(hello).padding()
     }
 }
 
